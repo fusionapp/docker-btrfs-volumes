6c0e9ff64b205353ecd8215667dfa81e0b0508f0
Tristan Seligmann <mithrandi@mithrandi.net>
2016-08-11 16:49:21 +0200

    Implement the skeleton of the command-line app.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,36 @@
 module Main where
 
+import Data.Monoid ((<>))
+import Network.Socket (Family(AF_UNIX), SocketType(Stream), SockAddr(SockAddrUnix), socket, bind, listen, maxListenQueue, close)
+import Network.Wai.Handler.Warp (runSettingsSocket, defaultSettings)
+import Options.Applicative (Parser, strOption, long, metavar, help, value, showDefault, execParser, info, helper, fullDesc, progDesc, header)
+
+parser :: Parser (FilePath, FilePath)
+parser = (,)
+  <$> strOption
+  ( long "socket"
+    <> metavar "PATH"
+    <> value "/run/docker/plugin/btrfs.sock"
+    <> showDefault
+    <> help "Socket path to serve the API at"
+  )
+  <*> strOption
+  ( long "basedir"
+    <> metavar "PATH"
+    <> help "Base directory to create btrfs subvolumes in"
+  )
+
 main :: IO ()
-main = return ()
+main = do
+  (socketPath, basedir) <- execParser opts
+  sock <- socket AF_UNIX Stream 0
+  bind sock (SockAddrUnix socketPath)
+  listen sock maxListenQueue
+  let app = undefined
+  runSettingsSocket defaultSettings sock app
+  close sock
+  where opts = info (helper <*> parser)
+          ( fullDesc
+            <> progDesc "Docker BTRFS volume plugin"
+            <> header "docker-btrfs-volumes - a Docker volume plugin"
+          )
